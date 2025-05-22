0b10a30f57a0738955994704849189663ef57e4f
RamaSandhy <rshandy07@gmail.com>
2025-05-23 02:06:20 +0700

    buat state HomePage dengan controller untuk gambar dan lokasi serta state untuk menyimpan path gambar dan alamat

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -4,48 +4,16 @@ import '../controllers/location_controller.dart';
 import '../widget/profile_widget.dart';
 
 class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
   @override
-  _HomePageState createState() => _HomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  String? alamatDipilih;
+  final ImageController _imageController = ImageController();
+  final LocationController _locationController = LocationController();
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text('Pilih Alamat'),
-                  IconButton(
-                    icon: const Icon(Icons.map, color: Colors.black),
-                    onPressed: () async {
-                      final result = await Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => MapPage(
-                          ),
-                        ),
-                      );
-                      // handle result here
-                    },
-                  ),
-                ],
-              ),
-
-              alamatDipilih == null
-                  ? const Text(' Tidak ada alamat yang dipilih')
-                  : Text(alamatDipilih!)
-            ],
-          ),
-        ),
-      ),
-    );
-  }
+  String? _imagePath;
+  String? _address;
 }
