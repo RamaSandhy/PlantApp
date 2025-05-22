412be7fea4afab2661b00c6bd10c8d47f46e81d1
RamaSandhy <rshandy07@gmail.com>
2025-05-23 01:48:19 +0700

    tambah conditional rendering untuk gambar profil dengan placeholder jika imagePath null

--- lib/widget/profile_widget.dart
+++ lib/widget/profile_widget.dart
@@ -5,4 +5,16 @@ class ProfileWidget extends StatelessWidget {
   final String? address;
 
   const ProfileWidget({super.key, this.imagePath, this.address});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CircleAvatar(
+          radius: 50,
+          backgroundImage: imagePath != null
+              ? FileImage(File(imagePath!))
+              : const AssetImage('assets/profile_placeholder.png')
+                  as ImageProvider,
+        ),
 }
