6595791e9003d3382030f50ee1883f41994ea144
RamaSandhy <rshandy07@gmail.com>
2025-05-23 00:34:39 +0700

    first commit

---
+++ android/app/src/main/kotlin/ramasandhy/id/getlocationmaps/MainActivity.kt
@@ -0,0 +1,5 @@
+package ramasandhy.id.getlocationmaps
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/home_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
+
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String? alamatDipilih;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('Pilih Alamat'),
+                  IconButton(
+                    icon: const Icon(Icons.map, color: Colors.black),
+                    onPressed: () async {
+                      final result = await Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => Scaffold(
+                          ),
+                        ),
+                      );
+                      // handle result here
+                    },
+                  ),
+                ],
+              ),
+
+              alamatDipilih == null
+                  ? const Text(' Tidak ada alamat yang dipilih')
+                  : Text(alamatDipilih!)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
