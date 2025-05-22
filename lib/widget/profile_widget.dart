5a9469157fb020153bd10d47eeab94b9f4c64d78
RamaSandhy <rshandy07@gmail.com>
2025-05-23 01:44:36 +0700

    tambah ProfileWidget untuk menampilkan gambar profil dan alamat pengguna

---
+++ lib/widget/profile_widget.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ProfileWidget extends StatelessWidget {
+  final String? imagePath;
+  final String? address;
+
+  const ProfileWidget({super.key, this.imagePath, this.address});
+}
